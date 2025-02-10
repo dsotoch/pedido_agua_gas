3a53fe79267878be6ccbb00e9cbb80871c816e14
Diego <dsoto6155@gmail.com>
2025-02-09 20:12:46 -0500

    d

--- resources/views/micuenta.blade.php
+++ resources/views/micuenta.blade.php
@@ -685,7 +685,7 @@
                                                     </div>
                                                 </div>
                                                 <img src="{{ asset('imagenes/cajas/cupons.png') }}" alt=""
-                                                class="absolute top-[55px] md:left-[25px] -right-2 w-[60px] h-[50px]">
+                                                class="absolute top-[55px] md:left-[25px] right-0 z-10 w-[60px] h-[50px]">
                                             
                                             </div>
                                         </div>
