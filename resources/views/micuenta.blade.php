6c022d96931ce074832bcf4ab1d692845a8a4272
Diego <dsoto6155@gmail.com>
2025-02-09 20:20:32 -0500

    vd

--- resources/views/micuenta.blade.php
+++ resources/views/micuenta.blade.php
@@ -271,27 +271,29 @@
                                         </div>
 
                                         @if (!empty($pedido->cupon))
-                                        <div class="absolute -top-0 right-28 md:right-10 h-[50px] ">
-                                            <div class="relative bg-transparent w-[150px] h-[190px] text-color-titulos-entrega group">
-                                                <div>
-                                                    <div
-                                                        class="z-50 -top-10 bg-green-400 left-0 hidden absolute group-hover:flex w-[250px] min-w-[250px] text-white p-2 rounded-md text-center">
-                                                        <p class="relative text-[14px] leading-[19.6px] ">
-                                                            <strong>¡Cupón Aplicado!</strong><br>
-                                                            Aplica un descuento de S/{{ $pedido->descuento }}
-                                                            equivalente al cupón #{{ $pedido->cupon }}
-                                                            en este pedido.
-                                                        </p>
+                                            <div class="absolute -top-0 right-28 md:right-10 h-[50px] ">
+                                                <div
+                                                    class="relative bg-transparent w-[150px] h-[190px] text-color-titulos-entrega group">
+                                                    <div>
                                                         <div
-                                                            class="absolute -bottom-[13px] left-[50%] md:left-[calc(50%-60px)] clip-v-shape h-[20px] w-[20px] bg-green-400">
+                                                            class="z-50 -top-10 bg-green-400 left-0 hidden absolute group-hover:flex w-[250px] min-w-[250px] text-white p-2 rounded-md text-center">
+                                                            <p class="relative text-[14px] leading-[19.6px] ">
+                                                                <strong>¡Cupón Aplicado!</strong><br>
+                                                                Aplica un descuento de S/{{ $pedido->descuento }}
+                                                                equivalente al cupón #{{ $pedido->cupon }}
+                                                                en este pedido.
+                                                            </p>
+                                                            <div
+                                                                class="absolute -bottom-[13px] left-1/2 md:left-1/2 -translate-x-1/2 md:-translate-x-[60px] clip-v-shape h-[20px] w-[20px] bg-green-400">
+                                                            </div>
+
                                                         </div>
                                                     </div>
+                                                    <img src="{{ asset('imagenes/cajas/cupons.png') }}" alt=""
+                                                        class="absolute top-[55px] md:left-[25px] right-0  md:w-[60px] w-[38px] h-[50px]">
+
                                                 </div>
-                                                <img src="{{ asset('imagenes/cajas/cupons.png') }}" alt=""
-                                                class="absolute top-[55px] md:left-[25px] right-0  md:w-[60px] w-[42px] h-[50px]">
-                                            
                                             </div>
-                                        </div>
                                         @endif
                                         @if ($pedido->entregaPromociones->count() > 0)
                                             <div class="absolute -top-14 right-0">
@@ -664,28 +666,29 @@
                                             </div>
                                         @endif
                                         @if (!empty($pedido->cupon))
-                                        <div class="absolute -top-0 right-28 md:right-10 h-[50px] ">
-                                            <div class="relative bg-transparent w-[150px] h-[190px] text-color-titulos-entrega group">
-                                                <div>
-                                                    <div
-                                                        class="z-50 -top-10 bg-green-400 left-0 hidden absolute group-hover:flex w-[250px] min-w-[250px] text-white p-2 rounded-md text-center">
-                                                        <p class="relative text-[14px] leading-[19.6px] ">
-                                                            <strong>¡Cupón Aplicado!</strong><br>
-                                                            Aplica un descuento de S/{{ $pedido->descuento }}
-                                                            equivalente al cupón #{{ $pedido->cupon }}
-                                                            en este pedido.
-                                                        </p>
+                                            <div class="absolute -top-0 right-28 md:right-10 h-[50px] ">
+                                                <div
+                                                    class="relative bg-transparent w-[150px] h-[190px] text-color-titulos-entrega group">
+                                                    <div>
                                                         <div
-                                                            class="absolute -bottom-[13px] left-[50%] md:left-[calc(50%-60px)] clip-v-shape h-[20px] w-[20px] bg-green-400">
+                                                            class="z-50 -top-10 bg-green-400 left-0 hidden absolute group-hover:flex w-[250px] min-w-[250px] text-white p-2 rounded-md text-center">
+                                                            <p class="relative text-[14px] leading-[19.6px] ">
+                                                                <strong>¡Cupón Aplicado!</strong><br>
+                                                                Aplica un descuento de S/{{ $pedido->descuento }}
+                                                                equivalente al cupón #{{ $pedido->cupon }}
+                                                                en este pedido.
+                                                            </p>
+                                                            <div
+                                                                class="absolute -bottom-[13px] left-1/2 md:left-1/2 -translate-x-1/2 md:-translate-x-[60px] clip-v-shape h-[20px] w-[20px] bg-green-400">
+                                                            </div>
+
                                                         </div>
                                                     </div>
+                                                    <img src="{{ asset('imagenes/cajas/cupons.png') }}" alt=""
+                                                        class="absolute top-[55px] md:left-[25px] right-0  md:w-[60px] w-[38px] h-[50px]">
+
                                                 </div>
-                                                <img src="{{ asset('imagenes/cajas/cupons.png') }}" alt=""
-                                                class="absolute top-[55px] md:left-[25px] right-0  md:w-[60px] w-[42px] h-[50px]">
-                                            
                                             </div>
-                                        </div>
-                                        
                                         @endif
                                     </div>
                                 </div>
