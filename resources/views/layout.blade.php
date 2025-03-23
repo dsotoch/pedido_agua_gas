3337cafa401a63678da602f398d602bb84cf715a
Diego <dsoto6155@gmail.com>
2025-03-22 20:56:57 -0500

    solucionando pwsa

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -60,36 +60,16 @@
     <link rel="apple-touch-icon" href="{{ asset('imagenes/images/icons/favicon-512x512.png') }}">
 
     <!-- Imágenes de inicio para diferentes dispositivos iOS -->
-    <link href="{{ asset('imagenes/images/icons/nautik.png') }}"
-        media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)"
-        rel="apple-touch-startup-image" />
-        <link href="{{ asset('imagenes/images/icons/nautik.png') }}"
-        media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)"
-        rel="apple-touch-startup-image" />
-        <link href="{{ asset('imagenes/images/icons/nautik.png') }}"
-        media="(device-width: 621px) and (device-height: 1104px) and (-webkit-device-pixel-ratio: 3)"
-        rel="apple-touch-startup-image" />
-        <link href="{{ asset('imagenes/images/icons/nautik.png') }}"
-        media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)"
-        rel="apple-touch-startup-image" />
-        <link href="{{ asset('imagenes/images/icons/nautik.png') }}"
-        media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)"
-        rel="apple-touch-startup-image" />
-        <link href="{{ asset('imagenes/images/icons/nautik.png') }}"
-        media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)"
-        rel="apple-touch-startup-image" />
-        <link href="{{ asset('imagenes/images/icons/nautik.png') }}"
-        media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)"
-        rel="apple-touch-startup-image" />
-        <link href="{{ asset('imagenes/images/icons/nautik.png') }}"
-        media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)"
-        rel="apple-touch-startup-image" />
-        <link href="{{ asset('imagenes/images/icons/nautik.png') }}"
-        media="(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)"
-        rel="apple-touch-startup-image" />
-    <link href="{{ asset('imagenes/images/icons/nautik.png') }}"
-        media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)"
-        rel="apple-touch-startup-image" />
+    <link href="{{ asset('imagenes/images/icons/splash-640x1136.png')}}" media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
+    <link href="{{ asset('imagenes/images/icons/splash-750x1334.png')}}" media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
+    <link href="{{ asset('imagenes/images/icons/splash-1242x2208.png')}}" media="(device-width: 621px) and (device-height: 1104px) and (-webkit-device-pixel-ratio: 3)" rel="apple-touch-startup-image" />
+    <link href="{{ asset('imagenes/images/icons/splash-1125x2436.png')}}" media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)" rel="apple-touch-startup-image" />
+    <link href="{{ asset('imagenes/images/icons/splash-828x1792.png')}}" media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
+    <link href="{{ asset('imagenes/images/icons/splash-1242x2688.png')}}" media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)" rel="apple-touch-startup-image" />
+    <link href="{{ asset('imagenes/images/icons/splash-1536x2048.png')}}" media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
+    <link href="{{ asset('imagenes/images/icons/splash-1668x2224.png')}}" media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
+    <link href="{{ asset('imagenes/images/icons/splash-1668x2388.png')}}" media="(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
+    <link href="{{ asset('imagenes/images/icons/splash-2048x2732.png')}}" media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
 
     <!-- Configuración del mosaico para Windows 8 -->
     <meta name="msapplication-TileColor" content="#ffffff">
