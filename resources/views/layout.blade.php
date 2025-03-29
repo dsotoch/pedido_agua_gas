17d838fd706603e426cffd60b1eee09c71ee06fc
Diego <dsoto6155@gmail.com>
2025-03-28 23:58:32 -0500

    sa

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -79,6 +79,20 @@
     @laravelPWA
 
     @yield('estilos')
+    <script type="text/javascript">
+    // Initialize the service worker
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('/firebase-messaging-sw.js', {
+            scope: '.'
+        }).then(function (registration) {
+            // Registration was successful
+            console.log('Laravel PWA: ServiceWorker registration successful with scope: ', registration.scope);
+        }, function (err) {
+            // registration failed :(
+            console.log('Laravel PWA: ServiceWorker registration failed: ', err);
+        });
+    }
+</script>
 
 </head>
 
